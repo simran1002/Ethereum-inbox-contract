1e71ed70750fa5c02128d8b8bf38f10fcfd712d0
Simran Yadav <simranyadav464@gmail.com>
2023-10-23 04:36:27 +0530

    Updated the test file

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,5 +2,85 @@ const assert = require('assert');
 const ganache = require('ganache');
 const { Web3 } = require('web3');
 const web3 = new Web3(ganache.provider());
- 
 
+let accounts;
+
+beforeEach(async () => {
+    //Get a list of all accounts
+    accounts =await web3.eth.getAccounts();
+
+    //USe one of those accounta to deploy
+    //the contract
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(accounts);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+class Car {
+    park() {
+        return 'stopped';
+    }
+
+    drive() {
+        return 'vroom';
+    }
+}
+
+let car;
+
+beforeEach(() => {
+    car = new Car();
+});
+
+describe('Car', () => {
+    it('can park', () => {
+        assert.equal(car.park(), 'stopped');
+    });
+
+    it('can drive', () => {
+        assert.equal(car.drive(), 'vroom');
+    });
+});
+*/
