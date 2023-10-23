a03dcf46043308541ae897ab4f2867ffd1f39c2a
Simran Yadav <simranyadav464@gmail.com>
2023-10-23 13:34:03 +0530

    Updated the test file

--- deploy.js
+++ deploy.js
@@ -1,5 +1,25 @@
+require('dotenv').config();
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const { Web3 } = require('web3');
-//updated web3 and hdwallet-provider imports added for convenience
+const  Web3  = require('web3');
+const { interface, bytecode} = require('./compile');
 
-// deploy code will go here
+
+const provider = new HDWalletProvider(
+    process.env.MNEMONIC,
+    process.env.INFURA_API_KEY
+);
+
+const web3 = new Web3(provider);
+
+const deploy  = async() => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas:'1000000', from: accounts[0]});
+
+    console.log('Contract deployed to', result.options.address);
+};
+deploy();
